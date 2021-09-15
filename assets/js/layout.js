ed1e6511ac7b5e6f7b426033ed3c16aec6891e92
Cameron Leverett <camrbo@gmail.com>
2021-09-15 11:45:30 -0700

    added some Js files and css

---
+++ assets/js/layout.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+	$('.nav').append(
+		`
+			<div class="nav-r1">
+      <a href="/whatsup.html">Whats up</a>
+				<a href="#">friends</a>
+				<a href="#">Invites</a>
+			</div>
+			<div class="nav-r2">
+				<a href="/nightplan.html">night plan</a>
+				<a href="#">night out</a>
+				<a href="#">previous</a>
+			</div>
+		`,
+	);
+});
---
+++ assets/js/nightplan.js
@@ -0,0 +1,3 @@
+$(document).ready(() => {
+	$('._main').append('<h1>Night Plan</h1>');
+});
