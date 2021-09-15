719b95d4524b02b0166af386d1fe71173648914b
Cameron Leverett <camrbo@gmail.com>
2021-09-15 12:46:32 -0700

    added styles to the navbar

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -5,11 +5,13 @@ $(document).ready(() => {
       <a href="/whatsup.html">Whats up</a>
 				<a href="#">friends</a>
 				<a href="#">Invites</a>
+        <i class="fas fa-home"></i>
 			</div>
 			<div class="nav-r2">
 				<a href="/nightplan.html">night plan</a>
 				<a href="#">night out</a>
 				<a href="#">previous</a>
+        <i class="fas fa-cog"></i>
 			</div>
 		`,
 	);
